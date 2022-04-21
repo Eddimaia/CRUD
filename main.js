6c2c98bb711091de24538ed2b4928ed2fcc5361d
Edson Moura <93019248+Eddimaia@users.noreply.github.com>
2022-04-21 01:43:30 -0300

    Buttons an

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+//FUNÇÃO DE INÍCIO
+function incioCrud(){
+  montarTabela(_DADOS.pistas_principal)
+  montarSelect()
+  document.querySelector('button').addEventListener('click',pesquisaSelect)
+}
+
 //READ
-function readCrud() {
-  let arrDadosPista = _DADOS.pistas_principal
+function montarTabela(arrDadosPista) {
   let accDadosTabela = ''
 
   arrDadosPista.forEach(a => accDadosTabela += 
@@ -20,13 +26,13 @@ function readCrud() {
   document.getElementById('tBodyRodovia').innerHTML = accDadosTabela
 }
 
-readCrud()
-
-
-
 //SELECT
-function selectCrud() {
-let arrDadosPista = _DADOS.pistas_principal
+function montarSelect() {
+
+  selectBuilder('concessionaria','selectConcessionaria')
+  selectBuilder('rodovia_uf','selectRodovia')
+  selectBuilder('sentido','selectSentido')
+/* let arrDadosPista = _DADOS.pistas_principal
 
 let txtSelectConcessionaria = ''
 let accConcessionaria = []
@@ -68,6 +74,7 @@ for (let i = 0; i < arrDadosPista.length; i++) {
   }
 }
 
+
 //SELECT SENTIDO
 for (let i = 0; i < arrDadosPista.length; i++) {
   
@@ -86,7 +93,152 @@ for (let i = 0; i < arrDadosPista.length; i++) {
 document.getElementById('selectConcessionaria').innerHTML = "<option selected>---</option>" + txtSelectConcessionaria
 document.getElementById('selectRodovia').innerHTML = "<option selected>---</option>" + txtSelectRodovia
 document.getElementById('selectSentido').innerHTML = "<option selected>---</option>" + txtSelectSentido
+ */
+}
+
+//CONSTRUTOR SELECT
+function selectBuilder(propriedade, ID) {
+
+  let arrDadosPista = _DADOS.pistas_principal
+  let txtSelect = ''
+  let accDado = []
+  
+  for (let i = 0; i < arrDadosPista.length; i++) {
+    
+    if (accDado.includes(arrDadosPista[i][propriedade]) === false){
+  
+      accDado.push(arrDadosPista[i][propriedade])
+  
+      txtSelect += 
+  
+        `<option values="${accDado[accDado.length -1]}">
+          ${accDado[accDado.length -1]}
+        </option>`
+    } 
+  }
+
+  document.getElementById(ID).innerHTML = "<option selected>---</option>" + txtSelect
 
 }
 
-selectCrud()
+
+//FUNÇÃO QUE SERÁ CHAMADA NO CLICK DO BOTÃO PARA IMPRIMIR OS ITENS SELECIONADOS NA TELA
+function pesquisaSelect() {
+  let arrDadosPista = _DADOS.pistas_principal
+  let accTxt = ''
+  let selectValorConcessionaria = document.getElementById('selectConcessionaria').value
+  let selectValorRodovia = document.getElementById('selectRodovia').value
+  let selectValorSentido = document.getElementById('selectSentido').value
+
+  if(selectValorConcessionaria !== '---' && selectValorRodovia === '---' && selectValorSentido === '---'){
+
+    for (let i = 0; i < arrDadosPista.length; i++) {
+      if (Object.is( selectValorConcessionaria , arrDadosPista[i].concessionaria) ==! false) {
+  
+        accTxt += 
+        
+        `<tr>
+          <td>${arrDadosPista[i].concessionaria}</td>
+          <td>${arrDadosPista[i].rodovia_uf}</td>
+          <td>${arrDadosPista[i].sentido}</td>
+          <td>${arrDadosPista[i].subtipo_de_pista}</td>
+          <td>${arrDadosPista[i].numero_de_faixas}</td>
+          <td>${arrDadosPista[i].km_m_inicial}</td>
+          <td>${arrDadosPista[i].km_m_final}</td>
+        </tr>`
+        
+      }
+
+  }
+
+  }else if (selectValorConcessionaria !== '---' && selectValorRodovia !== '---' && selectValorSentido === '---' ){
+
+    for (let i = 0; i < arrDadosPista.length; i++) {
+      if (Object.is( selectValorConcessionaria , arrDadosPista[i].concessionaria) ==! false && Object.is( selectValorRodovia , arrDadosPista[i].rodovia_uf) ==! false) {
+  
+        accTxt += 
+        
+        `<tr>
+          <td>${arrDadosPista[i].concessionaria}</td>
+          <td>${arrDadosPista[i].rodovia_uf}</td>
+          <td>${arrDadosPista[i].sentido}</td>
+          <td>${arrDadosPista[i].subtipo_de_pista}</td>
+          <td>${arrDadosPista[i].numero_de_faixas}</td>
+          <td>${arrDadosPista[i].km_m_inicial}</td>
+          <td>${arrDadosPista[i].km_m_final}</td>
+        </tr>`
+        
+      }
+
+  }
+
+  }else if (selectValorConcessionaria !== '---' && selectValorRodovia !== '---' && selectValorSentido !== '---'){
+
+    for (let i = 0; i < arrDadosPista.length; i++) {
+      if (Object.is( selectValorConcessionaria , arrDadosPista[i].concessionaria) ==! false && Object.is( selectValorRodovia , arrDadosPista[i].rodovia_uf) ==! false && Object.is( selectValorSentido , arrDadosPista[i].sentido) ==! false) {
+  
+        accTxt += 
+        
+        `<tr>
+          <td>${arrDadosPista[i].concessionaria}</td>
+          <td>${arrDadosPista[i].rodovia_uf}</td>
+          <td>${arrDadosPista[i].sentido}</td>
+          <td>${arrDadosPista[i].subtipo_de_pista}</td>
+          <td>${arrDadosPista[i].numero_de_faixas}</td>
+          <td>${arrDadosPista[i].km_m_inicial}</td>
+          <td>${arrDadosPista[i].km_m_final}</td>
+        </tr>`
+        
+      }
+
+  }
+
+  }else if(selectValorConcessionaria !== '---' && selectValorRodovia === '---' && selectValorSentido !== '---'){
+    
+    for (let i = 0; i < arrDadosPista.length; i++) {
+      if (Object.is( selectValorConcessionaria , arrDadosPista[i].concessionaria) ==! false && Object.is( selectValorSentido , arrDadosPista[i].sentido) ==! false) {
+  
+        accTxt += 
+        
+        `<tr>
+          <td>${arrDadosPista[i].concessionaria}</td>
+          <td>${arrDadosPista[i].rodovia_uf}</td>
+          <td>${arrDadosPista[i].sentido}</td>
+          <td>${arrDadosPista[i].subtipo_de_pista}</td>
+          <td>${arrDadosPista[i].numero_de_faixas}</td>
+          <td>${arrDadosPista[i].km_m_inicial}</td>
+          <td>${arrDadosPista[i].km_m_final}</td>
+        </tr>`
+        
+      }
+
+  }
+
+  }else{
+
+    arrDadosPista.forEach(a => accTxt += 
+
+      `<tr>
+        <td>${a.concessionaria}</td>
+        <td>${a.rodovia_uf}</td>
+        <td>${a.sentido}</td>
+        <td>${a.subtipo_de_pista}</td>
+        <td>${a.numero_de_faixas}</td>
+        <td>${a.km_m_inicial}</td>
+        <td>${a.km_m_final}</td>
+      </tr>`
+  
+      )
+
+  }
+
+  document.getElementById('tBodyRodovia').innerHTML = ''
+  document.getElementById('tBodyRodovia').innerHTML = accTxt
+
+  
+}
+
+
+
+
+incioCrud()
