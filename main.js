527b8f91ca59ca57ef3fd6de25be220cf52e241c
Edson Moura <93019248+Eddimaia@users.noreply.github.com>
2022-04-21 00:44:02 -0300

    Select and Bootstrap
    Added Bootstrap to style of the table and select boxes to filter the data. Some changes in the structure of the code as the use of template string to a more readble code.

--- main.js
+++ main.js
@@ -1,35 +1,92 @@
-let arr = _DADOS.pistas_principal
-let a = []
+//READ
+function readCrud() {
+  let arrDadosPista = _DADOS.pistas_principal
+  let accDadosTabela = ''
 
-function readCRUD() {
-  for (let i = 0; i < arr.length; i++) {
-    a +=
-      '<tr>' +
-      '<td>' +
-      arr[i].concessionaria +
-      '</td>' +
-      '<td>' +
-      arr[i].rodovia_uf +
-      '</td>' +
-      '<td>' +
-      arr[i].sentido +
-      '</td>' +
-      '<td>' +
-      arr[i].subtipo_de_pista +
-      '</td>' +
-      '<td>' +
-      arr[i].numero_de_faixas +
-      '</td>' +
-      '<td>' +
-      arr[i].km_m_inicial +
-      '</td>' +
-      '<td>' +
-      arr[i].km_m_final +
-      '</td>' +
-      '</tr>'
-  }
+  arrDadosPista.forEach(a => accDadosTabela += 
 
-  document.getElementById('obj').innerHTML = a
+    `<tr>
+      <td>${a.concessionaria}</td>
+      <td>${a.rodovia_uf}</td>
+      <td>${a.sentido}</td>
+      <td>${a.subtipo_de_pista}</td>
+      <td>${a.numero_de_faixas}</td>
+      <td>${a.km_m_inicial}</td>
+      <td>${a.km_m_final}</td>
+    </tr>`
+
+    )
+
+  document.getElementById('tBodyRodovia').innerHTML = accDadosTabela
 }
 
-readCRUD()
+readCrud()
+
+
+
+//SELECT
+function selectCrud() {
+let arrDadosPista = _DADOS.pistas_principal
+
+let txtSelectConcessionaria = ''
+let accConcessionaria = []
+
+let txtSelectRodovia = ''
+let accRodovia = []
+
+let txtSelectSentido = ''
+let accSentido = []
+
+//SELECT CONCESSIONARIA
+for (let i = 0; i < arrDadosPista.length; i++) {
+  
+  if (accConcessionaria.includes(arrDadosPista[i].concessionaria) === false){
+
+    accConcessionaria.push(arrDadosPista[i].concessionaria)
+
+    txtSelectConcessionaria += 
+
+      `<option values="${accConcessionaria[accConcessionaria.length -1]}">
+        ${accConcessionaria[accConcessionaria.length -1]}
+      </option>`
+  } 
+}
+
+
+//SELECT RODOVIA
+for (let i = 0; i < arrDadosPista.length; i++) {
+  
+  if (accRodovia.includes(arrDadosPista[i].rodovia_uf) === false){
+
+    accRodovia.push(arrDadosPista[i].rodovia_uf)
+
+    txtSelectRodovia += 
+
+      `<option values="${accRodovia[accRodovia.length -1]}">
+        ${accRodovia[accRodovia.length -1]}
+      </option>`
+  }
+}
+
+//SELECT SENTIDO
+for (let i = 0; i < arrDadosPista.length; i++) {
+  
+  if (accSentido.includes(arrDadosPista[i].sentido) === false){
+
+    accSentido.push(arrDadosPista[i].sentido)
+
+    txtSelectSentido += 
+    
+      `<option values="${accSentido[accSentido.length -1]}">
+        ${accSentido[accSentido.length -1]}
+      </option>`
+  } 
+}
+
+document.getElementById('selectConcessionaria').innerHTML = "<option selected>---</option>" + txtSelectConcessionaria
+document.getElementById('selectRodovia').innerHTML = "<option selected>---</option>" + txtSelectRodovia
+document.getElementById('selectSentido').innerHTML = "<option selected>---</option>" + txtSelectSentido
+
+}
+
+selectCrud()
