3d17e528c0a19d9a35c667afd5d6de860785dc23
Edson Moura <93019248+Eddimaia@users.noreply.github.com>
2022-04-27 23:56:43 -0300

    Added a Header tag and another type of filter
    Now, the selects reserved to "Rodovia" and "Sentido" come disabled when the page starts, whenever that the input of "Concessionaria" changes and its been diferent of "---", those two selects turns to enabled and the inputs are correlated with the contents that corresponds to the concessionaria that has been chosen.

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 //FUNÇÃO DE INÍCIO
 function incioCrud(){
+  
   montarTabela(_DADOS.pistas_principal)
-  montarSelect()
+
+
+  document.getElementById('selectConcessionaria').addEventListener('change', function (){
+
+    filterSelect('rodovia_uf','selectRodovia')
+    filterSelect('sentido','selectSentido')
+
+  })
+
+  montarSelects('concessionaria','selectConcessionaria')
+
   document.querySelector('button').addEventListener('click',pesquisaSelect)
+
+  document.getElementById('selectRodovia').setAttribute('disabled', '')
+  document.getElementById('selectSentido').setAttribute('disabled', '')
+
+  document.getElementById('selectConcessionaria').addEventListener('change',disabledSelect)
 }
 
 //READ
@@ -26,18 +42,10 @@ function montarTabela(arrDadosPista) {
   document.getElementById('tBodyRodovia').innerHTML = accDadosTabela
 }
 
-//SELECT
-function montarSelect() {
+//FUNÇÃO CONSTRUTORA DOS SELECT
+function montarSelects(propriedade, ID) {
 
-  selectBuilder('concessionaria','selectConcessionaria')
-  selectBuilder('rodovia_uf','selectRodovia')
-  selectBuilder('sentido','selectSentido')
-}
-
-//CONSTRUTOR SELECT
-function selectBuilder(propriedade, ID) {
-
-  let arrDadosPista = _DADOS.pistas_principal
+  const arrDadosPista = _DADOS.pistas_principal
   let txtSelect = ''
   let accDado = []
   
@@ -47,18 +55,31 @@ function selectBuilder(propriedade, ID) {
   
       accDado.push(arrDadosPista[i][propriedade])
   
-      txtSelect += 
-  
-        `<option values="${accDado[accDado.length -1]}">
-          ${accDado[accDado.length -1]}
-        </option>`
+      txtSelect += `<option values="${accDado[accDado.length -1]}">${accDado[accDado.length -1]}</option>`
     } 
   }
-
   document.getElementById(ID).innerHTML = "<option selected>---</option>" + txtSelect
-
 }
 
+//FUNÇÃO FILTRO PARA MONTAR OS SELECTS DE RODOVIA E SENTIDO BASEADO NO INPUT DO SELECT DE CONCESSIONARIA
+function filterSelect(propriedade, ID) {
+
+  const arrDadosPista = _DADOS.pistas_principal
+  const selectConcessionariaValor = document.getElementById('selectConcessionaria').value
+  let txtSelect = ''
+  let accDado = []
+
+  for (let i = 0; i < arrDadosPista.length; i++) {
+    
+    if (accDado.includes(arrDadosPista[i][propriedade]) === false && selectConcessionariaValor === arrDadosPista[i].concessionaria){
+  
+      accDado.push(arrDadosPista[i][propriedade])
+  
+      txtSelect += `<option values="${accDado[accDado.length -1]}">${accDado[accDado.length -1]}</option>`
+    } 
+  }
+  document.getElementById(ID).innerHTML = "<option selected>---</option>" + txtSelect
+}
 
 //FUNÇÃO QUE SERÁ CHAMADA NO CLICK DO BOTÃO PARA IMPRIMIR OS ITENS SELECIONADOS NA TELA
 function pesquisaSelect() {
@@ -66,7 +87,7 @@ function pesquisaSelect() {
   let selectValorConcessionaria = document.getElementById('selectConcessionaria').value
   let selectValorRodovia = document.getElementById('selectRodovia').value
   let selectValorSentido = document.getElementById('selectSentido').value
-
+  
   document.getElementById('tBodyRodovia').innerHTML = _DADOS.pistas_principal
   .filter( x => 
     ( selectValorConcessionaria === '---' || x.concessionaria === selectValorConcessionaria )
@@ -83,44 +104,25 @@ function pesquisaSelect() {
         <td>${x.km_m_final}</td>
       </tr>` )
       .join("")
+  
 
 }
 
+//FUNÇÃO QUE DESABILITA OU HABILITA OS SELECTS DE RODOVIA E SENTIDO BASEADO NO INPUT DO SELECT DE CONCESSIONÁRIA
+function disabledSelect() {
 
-/* function pesquisaSelect2() {
+  if (document.getElementById('selectConcessionaria').value === '---') {
+
+    document.getElementById('selectRodovia').setAttribute('disabled', '')
+    document.getElementById('selectSentido').setAttribute('disabled', '')
+    
+  } else {
+
+    document.getElementById('selectRodovia').removeAttribute('disabled', '')
+    document.getElementById('selectSentido').removeAttribute('disabled', '')
   
-  let arrDadosPista = _DADOS.pistas_principal
-  let accTxt = ''
-  let selectValorConcessionaria = document.getElementById('selectConcessionaria').value
-  let selectValorRodovia = document.getElementById('selectRodovia').value
-  let selectValorSentido = document.getElementById('selectSentido').value
-  let arrFiltrado = []
-
-    arrFiltrado = arrDadosPista.filter( x => 
-    ( selectValorConcessionaria === '---' || x.concessionaria === selectValorConcessionaria )
-    && ( selectValorRodovia === '---' || x.rodovia_uf === selectValorRodovia )
-    && ( selectValorSentido === '---' || x.sentido === selectValorSentido ))
-
-    arrFiltrado.forEach(x => accTxt += 
-
-      `<tr>
-        <td>${x.concessionaria}</td>
-        <td>${x.rodovia_uf}</td>
-        <td>${x.sentido}</td>
-        <td>${x.subtipo_de_pista}</td>
-        <td>${x.numero_de_faixas}</td>
-        <td>${x.km_m_inicial}</td>
-        <td>${x.km_m_final}</td>
-      </tr>`
+} 
   
-      )
-
-
-  document.getElementById('tBodyRodovia').innerHTML = accTxt
-
-  
-} */
-
-
+}
 
 incioCrud()
